691e0eb3ffc786c7343581911aac5eed62cff5fb
AlejandroBastos1 <163174740+AlejandroBastos1@users.noreply.github.com>
2024-05-13 00:29:31 +0200

    update backend

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -17,6 +17,8 @@ import { dirname } from 'path';
 
 const app = express();
 
+
+
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -4,7 +4,6 @@ import { port } from './config.js';
 
 
 connectDB();
-console.log(port)
 app.listen( port ,() => {
 console.log(`El servidor esta escuchando en el puerto ${port}`)
 });
